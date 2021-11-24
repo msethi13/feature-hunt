3e257315e6a382a448a1359bb1515c3e67bb21b6
elric97 <sharma.rachit882@gmail.com>
2021-11-24 17:59:13 -0500

    Revert "add new fields in form"
    This reverts commit dde85d4a431fab74c2299b9a5bb8f972b87351a4.

--- src/Components/ProjectForm.js
+++ src/Components/ProjectForm.js
@@ -4,10 +4,6 @@ import styled from "styled-components";
 import TextField from "@mui/material/TextField";
 import Button from '@mui/material/Button';
 import Service from "../Service";
-import FilledInput from '@mui/material/FilledInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
 
 const Styles = styled.div`
  background: #218888;
@@ -93,7 +89,6 @@ function ProjectForm() {
   const [user, setUser] = React.useState([""]);
   const [tags, setTags] = React.useState("");
   const [country, setCountry] = React.useState("");
-  const [funding, setFunding] = React.useState("");
 
   React.useEffect(() => {
     setUser(ReactSession.get("username"));
@@ -119,18 +114,12 @@ function ProjectForm() {
     setCountry(e.target.value);
   }
 
-  const handleFundingChange = (e) => {
-    setFunding(e.target.value);
-  }
-
-
   const handleSubmit = (event) => {
     const form = new FormData();
     form.append("productName", name);
     form.append("productDescription", description);
     form.append("lauchCountry", country);
     form.append("imageUrl", imageURL);
-    form.append("initialFunding", imageURL);
     form.append("email", user);
     form.append("tags", tags);
     Service.post("addProduct", form)
@@ -208,18 +197,6 @@ function ProjectForm() {
                   fullWidth
                 />
                 <br />
-                <label>Initial Funding</label>
-                <FormControl fullWidth variant="filled">
-                  <FilledInput
-                    id="filled-adornment-amount"
-                    value={funding}
-                    onChange={handleFundingChange}
-                    startAdornment={<InputAdornment>$</InputAdornment>}
-                    multiline
-                    maxRows={1}
-                  />
-              </FormControl>
-              <br />
               <label>Tags</label>
                 <TextField
                   id="filled-basic"
