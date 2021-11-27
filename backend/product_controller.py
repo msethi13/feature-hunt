950e780e9a6ad0e7454d1071ec9e5e63486e9391
elric97 <sharma.rachit882@gmail.com>
2021-11-27 15:08:56 -0500

    Initial changes for image upload

--- backend/product_controller.py
+++ backend/product_controller.py
@@ -5,37 +5,65 @@ from flask import request
 from app import app
 from db_init import product_records
 import datetime
+from werkzeug.utils import secure_filename
+import boto3
+from botocore.client import Config
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id="AKIA6RGMAFYCJPCLFAUS",
+    aws_secret_access_key="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",
+    region_name='us-east-1',
+    config=Config(signature_version='s3v4')
+)
+
 
 #################################################################################
-##       Function: add_product
-##       Description: This post request is used to gather all the information from
-##                    the project form and send it to the database to be stored
-##       Inputs:
-##           - NA
-##       Outputs:
-##           - Returns true or false if new project is able to be added
+#       Function: add_product
+#       Description: This post request is used to gather all the information from
+#                    the project form and send it to the database to be stored
+#       Inputs:
+#           - NA
+#       Outputs:
+#           - Returns true or false if new project is able to be added
 #################################################################################
 @app.route("/addProduct", methods=['Post'])
 def add_product():
+    try:
+        product_name = request.form.get("productName")
+        product_description = request.form.get("productDescription")
+        image_url = request.form.get("imageUrl")
+        email = request.form.get("email")
+        tags = request.form.get("tags").split(' ')
+        img = request.files['file']
 
-        try:
-            product_name = request.form.get("productName")
-            product_description = request.form.get("productDescription")
-            image_url = request.form.get("imageUrl")
-            email = request.form.get("email")
-            tags = request.form.get("tags").split(' ')
+        if img:
+            file_name = secure_filename(img.filename)
+            s3.upload_fileobj(
+                img,
+                "feature-hunt",
+                file_name,
+                ExtraArgs={
+                    "ContentType": img.content_type  # Set appropriate content type as per the file
+                }
+            )
 
-            feature_dict = {'id': 2, 'text': 'feature-1', 'votes': 1, 'timestamp': '1234567', 'tags': ['tag1']}
+            response = s3.generate_presigned_url('get_object',
+                                                 Params={'Bucket': "feature-hunt", 'Key': img.filename},
+                                                 ExpiresIn=500)
 
-            product_input = {'name': product_name, 'description': product_description,
-                             'image_url': image_url, 'users': [email], 'tags': tags, 'features': feature_dict}
+            print(response)
 
+        feature_dict = {'id': 2, 'text': 'feature-1', 'votes': 1, 'timestamp': '1234567', 'tags': ['tag1']}
 
-            product_records.insert_one(product_input)
+        product_input = {'name': product_name, 'description': product_description,
+                         'image_url': image_url, 'users': [email], 'tags': tags, 'features': feature_dict}
 
-            return jsonify(success=True)
-        except:
-            return jsonify(success=False)
+        # product_records.insert_one(product_input)
+
+        return jsonify(success=True)
+    except:
+        return jsonify(success=False)
 
 # @app.route("/<productName>/addFeature", method=['Post'])
 # def addFeature(productName):
