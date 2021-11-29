fdc1efcefdb6f9e62426aeb605b0ddb9c2a077fb
elric97 <sharma.rachit882@gmail.com>
2021-11-29 18:05:01 -0500

    Fix test which all expected user to be logged in initially

--- src/__tests__/Header.tests.js
+++ src/__tests__/Header.tests.js
@@ -8,6 +8,7 @@ import "@testing-library/jest-dom/extend-expect";
 import Header from "../Components/Header";
 
 import "../setupTests";
+import Login from "../Components/Login";
 
 /**
  * This file tests Header.js
@@ -39,14 +40,12 @@ data-testid="TEXT" -- short description
 describe("Header tests", () => {
   it("renders header: screen checks 1", () => {
     render(<Header />, { wrapper: MemoryRouter });
-    const submitProject = screen.getByText(/Submit Project/i);
     const roadmap = screen.getByText(/Roadmap/i);
     const feedback = screen.getByText(/Feedback/i);
 
-    const logout = screen.getByText(/LogOut/i);
-    expect(logout).toBeInTheDocument();
+    // const logout = screen.getByText(/LogOut/i);
+    // expect(logout).toBeInTheDocument();
 
-    expect(submitProject).toBeInTheDocument();
     expect(roadmap).toBeInTheDocument();
     expect(feedback).toBeInTheDocument();
   });
@@ -79,37 +78,37 @@ describe("Header tests", () => {
     // home button
     const home = getByTestId("header_home");
 
-    const submittext = getByText(/Submit Project/i);
-    expect(submittext).toBeInTheDocument();
+    // const submittext = getByText(/Submit Project/i);
+    // expect(submittext).toBeInTheDocument();
 
     // links
-    const submit = getByTestId("header_sub");
-    const dash = getByTestId("header_dash");
+    // const submit = getByTestId("header_sub");
+    // const dash = getByTestId("header_dash");
     const feedback = getByTestId("header_fb");
     const roadmap = getByTestId("header_rm");
 
     const links = getByTestId("header_links");
-    expect(links.children.length).toBe(4); // check number of links
+    expect(links.children.length).toBe(2); // check number of links
 
     expect(history.length).toBe(2);
     fireEvent.click(home);
+    // expect(history.length).toBe(3);
+    // fireEvent.click(dash);
     expect(history.length).toBe(3);
-    fireEvent.click(dash);
-    expect(history.length).toBe(4);
     fireEvent.click(roadmap);
-    expect(history.length).toBe(5);
-    fireEvent.click(submit);
-    expect(history.length).toBe(6);
+    // expect(history.length).toBe(5);
+    // fireEvent.click(submit);
+    expect(history.length).toBe(4);
     fireEvent.click(feedback);
-    expect(history.length).toBe(7);
+    expect(history.length).toBe(5);
     expect(history.location.pathname).toBe("/feedback");
 
-    const logout = getByRole("button", { name: /LogOut/i }); // id: "logout_header"
-    expect(logout).toBeInTheDocument();
-
-    fireEvent.click(logout);
-    expect(history.length).toBe(8);
-    expect(history.location.pathname).toBe("/");
+    // const logout = getByRole("button", { name: /LogOut/i }); // id: "logout_header"
+    // expect(logout).toBeInTheDocument();
+    //
+    // fireEvent.click(logout);
+    // expect(history.length).toBe(7);
+    // expect(history.location.pathname).toBe("/");
 
     const nothere = queryByText(/Your Projects/i); // ensure is absent
     expect(nothere).not.toBeInTheDocument();
