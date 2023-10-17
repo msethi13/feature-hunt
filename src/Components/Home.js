3ecc5177a23aaca5150e9f4594ac7892c55431c7
sid-ncsu <ssshah26@ncsu.edu>
2023-10-17 14:30:32 -0400

    trending products feature

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,36 +13,85 @@ import Service from '../Service';
 const Home = ({query}) => {
   const [sortBy, setSortBy] = useState('timestamp');
   const [products, setProducts] = useState([]);
+  const [topProducts, setTopProducts] = useState('');
+  
+  const findTopProducts = (products) => {
+    const topProducts=[];
+    products.forEach((product) => { 
+    topProducts.push([product.views.length,product.name]);
+    topProducts.sort((a, b) => b[0] - a[0]);
+    });
+    return [topProducts[0][1],topProducts[1][1],topProducts[2][1]];
+  };
+
   useEffect(() => {
-    Service.get('products', "query=" + query).then(products => setProducts(products));
+    Service.get('products', "query=" + query).then(products => {
+      setProducts(products);
+      setTopProducts(findTopProducts(products));
+    });
+
   }, [query]);
 
+  
   return (
-    <div className="container">
+  <div className="container">
+  <div className="row" style={{ display: 'flex' }}>
+    <div className="left-column" style={{ width: '40%' }}>
       <div className="child">
         <div className="product-title">
           <h3 data-testid="home_header">PRODUCTS</h3>
           <div className="sort">
-            <p 
-            className={sortBy === 'votes' ? 'highlight' : ''}
-            data-testid="home_sortpop"
-            onClick={() => setSortBy('votes')}>POPULAR
+            <p
+              className={sortBy === 'votes' ? 'highlight' : ''}
+              data-testid="home_sortpop"
+              onClick={() => setSortBy('votes')}
+            >
+              POPULAR
             </p>
             <p> | </p>
-            <p 
-            className={sortBy === 'timestamp' ? 'highlight' : ''} 
-            data-testid="home_sorttime"
-            onClick={() => setSortBy('timestamp')}>LATEST
+            <p
+              className={sortBy === 'timestamp' ? 'highlight' : ''}
+              data-testid="home_sorttime"
+              onClick={() => setSortBy('timestamp')}
+            >
+              LATEST
             </p>
           </div>
         </div>
+        {products
+          .map((p, index) => {
+            p['index'] = index;
+            return p;
+          })
+          .sort((p1, p2) => p2[sortBy] - p1[sortBy])
+          .map((product) => (
+            <ProductTile
+              key={product.id}
+              products={products}
+              index={product.index}
+              setProducts={setProducts}
+            />
+          ))}
       </div>
-      {products
-      .map((p, index) => { p['index'] = index; return p; })
-      .sort((p1, p2) => p2[sortBy] - p1[sortBy])
-      .map((product) => <ProductTile key={product.id} products={products} index={product.index} setProducts={setProducts} />, setProducts)
-      }
     </div>
+
+    <div className="column" style={{ width: '50%', textAlign: 'right' }}>
+    <div class="row">
+        <p>TOP PRODUCTS</p>
+    </div>
+    <div class="row">
+        <p>{topProducts[0]}</p>
+    </div>
+    <div class="row">
+        <p>{topProducts[1]}</p>
+    </div>
+    <div class="row">
+        <p>{topProducts[2]}</p>
+    </div>
+    </div>
+  </div>
+</div>
+
   );
 };
 
