0f7d78e92ce0157c7872440d9881297e326dc869
“MitaliSethi” <mitalisethi13@gmail.com>
2023-10-18 19:47:21 -0400

    top product changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,6 +14,7 @@ const Home = ({query}) => {
   const [sortBy, setSortBy] = useState('timestamp');
   const [products, setProducts] = useState([]);
   const [topProducts, setTopProducts] = useState('');
+  const [numberOfProducts, setNumberOfProducts] = useState(0);
   
   const findTopProducts = (products) => {
     const topProducts=[];
@@ -21,78 +22,134 @@ const Home = ({query}) => {
     topProducts.push([product.views.length,product.name]);
     topProducts.sort((a, b) => b[0] - a[0]);
     });
+    console.log(topProducts);
     return [topProducts[0][1],topProducts[1][1],topProducts[2][1]];
   };
 
   useEffect(() => {
     Service.get('products', "query=" + query).then(products => {
       setProducts(products);
-      setTopProducts(findTopProducts(products));
+      setNumberOfProducts(products.length);
+      if (numberOfProducts >= 3){
+        setTopProducts(findTopProducts(products));
+        console.log(topProducts);
+      }
     });
 
   }, [query]);
 
-  
-  return (
-  <div className="container">
-  <div className="row" style={{ display: 'flex' }}>
-    <div className="left-column" style={{ width: '40%' }}>
-      <div className="child">
-        <div className="product-title">
-          <h3 data-testid="home_header">PRODUCTS</h3>
-          <div className="sort">
-            <p
-              className={sortBy === 'votes' ? 'highlight' : ''}
-              data-testid="home_sortpop"
-              onClick={() => setSortBy('votes')}
-            >
-              POPULAR
-            </p>
-            <p> | </p>
-            <p
-              className={sortBy === 'timestamp' ? 'highlight' : ''}
-              data-testid="home_sorttime"
-              onClick={() => setSortBy('timestamp')}
-            >
-              LATEST
-            </p>
+  if (numberOfProducts>=3)
+  {
+    return (
+      <div className="container">
+      <div className="row" style={{ display: 'flex' }}>
+        <div className="left-column" style={{ width: '40%' }}>
+          <div className="child">
+            <div className="product-title">
+              <h3 data-testid="home_header">PRODUCTS</h3>
+              <div className="sort">
+                <p
+                  className={sortBy === 'votes' ? 'highlight' : ''}
+                  data-testid="home_sortpop"
+                  onClick={() => setSortBy('votes')}
+                >
+                  POPULAR
+                </p>
+                <p> | </p>
+                <p
+                  className={sortBy === 'timestamp' ? 'highlight' : ''}
+                  data-testid="home_sorttime"
+                  onClick={() => setSortBy('timestamp')}
+                >
+                  LATEST
+                </p>
+              </div>
+            </div>
+            {products
+              .map((p, index) => {
+                p['index'] = index;
+                return p;
+              })
+              .sort((p1, p2) => p2[sortBy] - p1[sortBy])
+              .map((product) => (
+                <ProductTile
+                  key={product.id}
+                  products={products}
+                  index={product.index}
+                  setProducts={setProducts}
+                />
+              ))}
           </div>
         </div>
-        {products
-          .map((p, index) => {
-            p['index'] = index;
-            return p;
-          })
-          .sort((p1, p2) => p2[sortBy] - p1[sortBy])
-          .map((product) => (
-            <ProductTile
-              key={product.id}
-              products={products}
-              index={product.index}
-              setProducts={setProducts}
-            />
-          ))}
+        
+        <div className="column" style={{ width: '50%', textAlign: 'right' }}>
+        <div class="row">
+            <p>TOP PRODUCTS</p>
+        </div>
+        <div class="row">
+            <p>{topProducts[0]}</p>
+        </div>
+        <div class="row">
+            <p>{topProducts[1]}</p>
+        </div>
+        <div class="row">
+            <p>{topProducts[2]}</p>
+        </div>
+        </div>
       </div>
     </div>
-
-    <div className="column" style={{ width: '50%', textAlign: 'right' }}>
-    <div class="row">
-        <p>TOP PRODUCTS</p>
+    
+      );
+  }
+  else
+  {
+    return (
+      <div className="container">
+      <div className="row" style={{ display: 'flex' }}>
+        <div className="left-column" style={{ width: '40%' }}>
+          <div className="child">
+            <div className="product-title">
+              <h3 data-testid="home_header">PRODUCTS</h3>
+              <div className="sort">
+                <p
+                  className={sortBy === 'votes' ? 'highlight' : ''}
+                  data-testid="home_sortpop"
+                  onClick={() => setSortBy('votes')}
+                >
+                  POPULAR
+                </p>
+                <p> | </p>
+                <p
+                  className={sortBy === 'timestamp' ? 'highlight' : ''}
+                  data-testid="home_sorttime"
+                  onClick={() => setSortBy('timestamp')}
+                >
+                  LATEST
+                </p>
+              </div>
+            </div>
+            {products
+              .map((p, index) => {
+                p['index'] = index;
+                return p;
+              })
+              .sort((p1, p2) => p2[sortBy] - p1[sortBy])
+              .map((product) => (
+                <ProductTile
+                  key={product.id}
+                  products={products}
+                  index={product.index}
+                  setProducts={setProducts}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="row">
-        <p>{topProducts[0]}</p>
-    </div>
-    <div class="row">
-        <p>{topProducts[1]}</p>
-    </div>
-    <div class="row">
-        <p>{topProducts[2]}</p>
-    </div>
-    </div>
-  </div>
-</div>
-
-  );
+    
+      );
+  }
+ 
 };
 
 export default Home;
